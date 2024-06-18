15b1c316419c466b4dcd68b0f1afcb2c1a53331f
Bevan Wishart <bevan.wishart@gmail.com>
2024-06-18 10:44:50 +1000

    Initial pass of setting up angular

---
+++ templates/articles.component.ts
@@ -0,0 +1,31 @@
+
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-articles',
+  templateUrl: './articles.component.html',
+  styleUrls: ['./articles.component.css']
+})
+export class ArticlesComponent implements OnInit {
+  articles: any[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    const token = localStorage.getItem('access_token');
+    this.http.get('https://YOUR_CORS_ENV.ddev.site/jsonapi/node/article', {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    }).subscribe((data: any) => {
+      if (data.data) {
+        this.articles = data.data;
+      } else {
+        console.log('No articles found.');
+      }
+    }, error => {
+      console.error('Error fetching data:', error);
+    });
+  }
+}
