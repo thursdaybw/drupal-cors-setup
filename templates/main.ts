80bd24d20d7c9ab00ccd56201786d33db8cbc4c3
Bevan Wishart <bevan.wishart@gmail.com>
2024-06-24 14:08:39 +1000

    ADd FormsModule to angular main configuration

--- templates/main.ts
+++ templates/main.ts
@@ -1,8 +1,10 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { provideHttpClient } from '@angular/common/http';
+import { importProvidersFrom } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 bootstrapApplication(AppComponent, {
-  providers: [provideHttpClient()]
+  providers: [provideHttpClient(), importProvidersFrom(FormsModule)]
 }).catch(err => console.error(err));
 
